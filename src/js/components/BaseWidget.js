d24a8a9048c41ff59973a7197b50c9abb5ffde33
maslana.m <maslana.m89@gmail.com>
2020-04-26 19:37:32 +0200

    Adding BaseWidget

---
+++ src/js/components/BaseWidget.js
@@ -0,0 +1,42 @@
+export class BaseWidget {
+  constructor(wrapperElem, initialValue){
+    const thisWidget = this;
+
+    thisWidget.dom = {};
+    thisWidget.dom.wrapper = wrapperElem;
+
+    thisWidget.correctValue = initialValue;
+  }
+  get value(){
+    const thisWidget = this;
+
+    return thisWidget.correctValue;
+  }
+  set value(assignedValue){
+    const thisWidget = this;
+
+    const newValue = thisWidget.parseValue(assignedValue);
+
+    if(newValue != thisWidget.correctValue && thisWidget.isValid(newValue)){
+      thisWidget.correctValue = newValue;
+      thisWidget.announce();
+    }
+
+    thisWidget.renderValue();
+  }
+  parseValue(newValue){
+    return parseInt(newValue);
+  }
+  isValid(newValue){
+    return !isNaN(newValue);
+  }
+  announce(){
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+
+    thisWidget.dom.wrapper.dispatchEvent(event);
+  }
+}
